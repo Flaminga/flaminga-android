a70512b0014b812f27155476572a81366442be54
Matthew Janes <mjanes@gmail.com>
2014-01-11 20:51:25 -0800

    Misc initializations

--- Flaminga/src/main/java/com/flaminga/client/activity/MainActivity.java
+++ Flaminga/src/main/java/com/flaminga/client/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.janes.flaminga;
+package com.flaminga.client.activity;
 
 import android.app.Activity;
 import android.app.ActionBar;
@@ -10,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+
+import com.flaminga.client.R;
 
 public class MainActivity extends Activity {
 
